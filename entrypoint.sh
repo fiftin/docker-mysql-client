bf12b1b40ea24968f4499a8a12806924dda107af
Gabor Kozma <gabo@kozmagabor.com>
2018-12-19 11:23:39 +0100

    fix password

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,15 @@ if [ "$VERBOSE" = "true" ]; then
 fi
 
 if [ "$MYSQL_PASSWORD" != "" ]; then
-	MYSQL_PASSWORD="-p$MYSQL_PASSWORD"
+	MYSQL_PASSWORD="--password=$MYSQL_PASSWORD"
 fi
 
 : "${MYSQL_USER:=root}"
 
 : "${MYSQL_HOST:=localhost}"
 
-exec mysql $MYSQL_VERBOSE -u $MYSQL_USER $MYSQL_PASSWORD -h$MYSQL_HOST -e "$(echo $@ | envtpl)"
+if [ "$MYSQL_DATABASE" != "" ]; then
+	MYSQL_DB="-D$MYSQL_DATABASE"
+fi
+
+exec mysql $MYSQL_VERBOSE -u $MYSQL_USER $MYSQL_PASSWORD $MYSQL_DB -h$MYSQL_HOST -e "$(echo $@ | envtpl)"
